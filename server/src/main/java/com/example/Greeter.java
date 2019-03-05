3d2bdbdebce15203a9a28de7eaf8a0b95b601d3c
lowerbo <48215491+lowerbo@users.noreply.github.com>
2019-03-05 15:24:25 +0100

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -7,6 +7,11 @@ public class Greeter {
   public Greeter() {
 
   }
+  
+  /**
+  * @param someone is name of a good personne
+  * @return afficher something right
+  */
 
   public final String greet(final String someone) {
     return String.format("Hello babacar, %s!", someone);
